0a55ec54c1cfb4899b0900a4bc8e9b966f386d46
Daniel Chae <daniel.jiwoong@gmail.com>
2025-01-08 21:02:53 +0900

    added nvim plugins

---
+++ .config/nvim/lua/plugins/canter.lua
@@ -0,0 +1,38 @@
+return {
+  "dchae/canter.nvim",
+  opts = {
+    runners = {
+      ["js"] = "node",
+      ["rb"] = "ruby",
+      ["py"] = "python",
+    },
+  },
+}
+
+-- local dev build
+-- return {
+--   "canter.nvim",
+--   dir = "/Users/dchae/github-dchae/canter.nvim",
+--
+--   opts = {
+--     runners = {
+--       ["js"] = "node",
+--       ["rb"] = "ruby",
+--       ["py"] = "python",
+--     },
+--     terminal = {
+--       type = "builtin",
+--       builtin_opts = {
+--         position = "vsplit", -- can be "vsplit", "split", or "float"
+--         escape_keymap = true, -- escape terminal mode with <Esc>
+--       },
+--       snacks_opts = {
+--         win = {
+--           position = "bottom",
+--           relative = "editor",
+--         },
+--         interactive = false,
+--       },
+--     },
+--   },
+-- }
