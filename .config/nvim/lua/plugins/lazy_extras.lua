6b1391d5a58cb40a9c89897f404ab624879ade90
Daniel Chae <daniel.jiwoong@gmail.com>
2025-02-10 23:25:34 +0900

    fresh install of lazyvim + aerial width opts

--- .config/nvim/lua/plugins/lazy_extras.lua
+++ .config/nvim/lua/plugins/lazy_extras.lua
@@ -21,6 +21,15 @@ return {
     },
   },
 
+  {
+    "stevearc/aerial.nvim",
+    opts = {
+      layout = {
+        min_width = { 20 },
+      },
+    },
+  },
+
   -- lazy load vscode style snippets to luasnip
   -- {
   --   require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.expand("../../snippets") } }),
